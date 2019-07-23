517fe73bb73c427820e17e277342fc96813dc933
GeovanyOjeda14 <geovanyojeda14@gmail.com>
2019-07-23 16:23:55 -0500

    implementado expotar certificado con observaciones

--- src/app/components/historial-citas-medico/historial-citas-medico.component.ts
+++ src/app/components/historial-citas-medico/historial-citas-medico.component.ts
@@ -7,6 +7,10 @@ import {
   CalendarView,
   CalendarMonthViewDay
 } from 'angular-calendar';
+import {
+  startOfDay,
+  addHours
+} from 'date-fns';
 
 import * as moment from 'moment';
 
@@ -31,6 +35,7 @@ const colors: any = {
 // servicios
 import { MedicoService } from '../../services/medico.service';
 import { UserService } from '../../services/user.service';
+import { PlatformLocation } from '@angular/common';
 
 
 @Component({
@@ -57,9 +62,13 @@ export class HistorialCitasMedicoComponent implements OnInit {
   public servicioSelect;
   public servicio;
   public medico_id;
+  public resEventos;
+  public infoPaciente;
 
-  constructor(private _userService : UserService, private _medicoService : MedicoService){
-    
+  constructor(private _userService : UserService, private _medicoService : MedicoService, location: PlatformLocation){
+    location.onPopState(() => {
+      document.getElementById('btn-cerrar-modal-info').click();
+    });
   }
 
   ngOnInit() {
@@ -91,18 +100,64 @@ export class HistorialCitasMedicoComponent implements OnInit {
   servicioSelecionado(ev) {
     this.servicio = ev.value;
     console.log(this.servicio);
-
-    this.getHistorial(this.servicio.categoria_idcategoria, this.servicio.id_servicios);
-  }
-
-  getHistorial(id_categoria, id_servicio) {
     let anio = moment(new Date).format('YYYY');
     let mes =  moment(new Date).format('M');
 
-    console.log(mes, anio, id_categoria, id_servicio);
+    this.getHistorial(this.servicio.categoria_idcategoria, this.servicio.id_servicios, anio, mes);
+  }
+
+  getHistorial(id_categoria, id_servicio, anio, mes) {
+    
+    this.loading = true;
+    this.events = [];
+    // console.log(mes, anio, id_categoria, id_servicio);
 
     this._medicoService.getHistorialCitasCalendar(mes, anio, this.medico_id, id_categoria, id_servicio).subscribe( (response) => {
       console.log(response);
+      this.resEventos = response;
+
+      if(this.resEventos.length >= 1) {
+
+        for(let i = 0; i < this.resEventos.length; i++) {
+          
+          let title = this.resEventos[i].nombre + ' ' + this.resEventos[i].apellidos;
+
+          let diaS = moment(this.resEventos[i].start).format('ddd');
+              let mesS = moment(this.resEventos[i].start).format('MMM');
+              let fechaS = moment(this.resEventos[i].start).format('DD-YYYY');
+              let horaS = moment.utc(this.resEventos[i].start).format('h:mm:ss');
+              let horaSs = moment.utc(this.resEventos[i].start).format('H');
+              let start = diaS + ' ' + mesS + ' ' + fechaS + ' ' +  horaS;
+              // d = horaS;
+
+              let diaE = moment(this.resEventos[i].end).format('ddd');
+              let mesE = moment(this.resEventos[i].end).format('MMM');
+              let fechaE = moment(this.resEventos[i].end).format('DD-YYYY');
+              let horaE = moment.utc(this.resEventos[i].end).format('h:mm:ss');
+              let horaEe = moment.utc(this.resEventos[i].end).format('H');
+
+              let end = diaE + ' ' + mesE + ' ' + fechaE + ' ' +  horaE;
+
+          
+          let info = {};
+
+              let apellidos = this.resEventos[i].apellidos;
+              let avatar = this.resEventos[i].avatar;
+              let cedula = this.resEventos[i].cedula;
+              let fecha_nacimiento = this.resEventos[i].fecha_nacimiento;
+              let nombre = this.resEventos[i].nombre;
+              let telefono = this.resEventos[i].telefono;
+              let id_eventos = this.resEventos[i].id_eventos;
+
+                info = {id : this.resEventos[i].id, tipo : 'usuario', apellidos : apellidos, avatar : avatar , cedula: cedula,
+                        fecha_nacimiento: fecha_nacimiento, nombre: nombre, telefono: telefono, id_eventos: id_eventos} ;
+              
+                        this.addEvent(title, start, end, horaSs, horaEe, info);
+        
+        }
+      }
+
+      this.loading = false;
     }, (err) => { 
       console.log(err);
     });
@@ -111,5 +166,56 @@ export class HistorialCitasMedicoComponent implements OnInit {
 
   closeOpenMonthViewDay(ev) {
     console.log(ev);
+    // console.log(moment(ev).format('YYYY'), moment(ev).format('M'));
+    this.getHistorial(this.servicio.categoria_idcategoria, this.servicio.id_servicios,moment(ev).format('YYYY') ,moment(ev).format('M'));
+  }
+
+  addEvent(title, start, end, horaInicio, horaFinal, info): void {
+    this.loading = true;
+    console.log('aqui');
+    this.events = [
+      ...this.events,
+      {
+        title: title,
+        start:  addHours(startOfDay(start), horaInicio),
+        end:  addHours(startOfDay(end), horaFinal),
+        color: colors.prevenir,
+        id : info,
+        draggable: false,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true
+        }
+
+        
+      }
+    ];
+
+
+    // this.events = [
+    //   ...this.events,
+    //   {
+    //     title: 'New event',
+    //     start: startOfDay(new Date()),
+    //     end: endOfDay(new Date()),
+    //     color: colors.red,
+    //     draggable: true,
+    //     resizable: {
+    //       beforeStart: true,
+    //       afterEnd: true
+    //     }
+    //   }
+    // ];
+
+
+    console.log(this.events);
+    this.loading = false;
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    document.getElementById('btn-abrir-modal-info').click();
+
+    this.infoPaciente = event;
+    console.log(event);
   }
 }
